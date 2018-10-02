b34a70fc98ccd23f6a71cea41957a4338bc4534c
christianrorholtmoe <34798018+christianrorholtmoe@users.noreply.github.com>
2018-10-02 21:03:42 +0000

    First commit on Alexa skill

---
+++ sayHelloLambdaForAlexa/Readme.md
@@ -0,0 +1,3 @@
+Build command - mvn assembly:assembly -DdescriptorId=jar-with-dependencies package
+
+Lambda Hander Class - com.example.howtodoinjava.alexa.SayHelloRequestStreamHandler
---
+++ sayHelloLambdaForAlexa/src/alexa/SayHelloRequestStreamHandler.java
@@ -0,0 +1,32 @@
+package alexa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.amazon.speech.speechlet.Speechlet;
+import com.amazon.speech.speechlet.lambda.SpeechletRequestStreamHandler;
+
+public class SayHelloRequestStreamHandler extends SpeechletRequestStreamHandler {
+
+	private static final Set<String> supportedApplicationIds;
+
+	static {
+		/*
+		 * This Id can be found on https://developer.amazon.com/edw/home.html#/
+		 * "Edit" the relevant Alexa Skill and put the relevant Application Ids
+		 * in this Set.
+		 */
+		supportedApplicationIds = new HashSet<String>();
+		supportedApplicationIds.add("amzn1.ask.skill.ff0d64bf-58e6-4b57-982f-9de85351ac3f");
+		System.out.println("Supported app ids : " + supportedApplicationIds);
+	}
+
+	public SayHelloRequestStreamHandler() {
+		super(new SayHelloSpeechlet(), supportedApplicationIds);
+	}
+
+	public SayHelloRequestStreamHandler(Speechlet speechlet, Set<String> supportedApplicationIds) {
+		super(speechlet, supportedApplicationIds);
+	}
+
+}
