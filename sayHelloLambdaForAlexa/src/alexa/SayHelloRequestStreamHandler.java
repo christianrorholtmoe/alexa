99d1cf2b83bc2b7222eddeee24d00ea5fdeaedcd
christianrorholtmoe <34798018+christianrorholtmoe@users.noreply.github.com>
2018-10-18 05:38:31 +0000

    Updated application Id

--- sayHelloLambdaForAlexa/src/alexa/SayHelloRequestStreamHandler.java
+++ sayHelloLambdaForAlexa/src/alexa/SayHelloRequestStreamHandler.java
@@ -17,7 +17,7 @@ public class SayHelloRequestStreamHandler extends SpeechletRequestStreamHandler 
 		 * in this Set.
 		 */
 		supportedApplicationIds = new HashSet<String>();
-		supportedApplicationIds.add("amzn1.ask.skill.53529ff6-4a8e-4282-b047-490c82a15fd5");
+		supportedApplicationIds.add("amzn1.ask.skill.8971b7fd-3a2e-4393-85c5-12c4e85cf2a6");
 		System.out.println("Supported app ids : " + supportedApplicationIds);
 	}
 
